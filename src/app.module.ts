c7059a00b9982885b1edffd80ab8756e04067332
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-03-10 21:17:35 +0100

    optimize imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { MyDocController } from "./api/v1/my-doc/controller/my-doc.controller";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { MyDocController } from './api/v1/my-doc/controller/my-doc.controller';
 import { MyDocService } from './api/v1/my-doc/services/my-doc.service';
 
 const v1Controllers = [MyDocController];
